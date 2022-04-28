f39b28c32c4a572cc95ad144649bad98f8466f4b
Bishr Jones <Bishrjones6@gmail.com>
2022-04-28 06:09:05 -0700

    loading message

--- src/components/workouts/indexWorkouts.js
+++ src/components/workouts/indexWorkouts.js
@@ -14,4 +14,11 @@ const IndexWorkouts = (props) => {
             })
             .catch(console.error)
     }, [])
+
+    // loading screen for the latency of the api call
+    if (!workouts) {
+        return <p>Loading...</p>
+    } else if (workouts.length=== 0){
+        return <p>Looks like there are no workouts, Let's add some!</p>
+    }
 }
