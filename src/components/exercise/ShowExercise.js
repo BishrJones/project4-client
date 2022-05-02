23641137ea60ca025a6b45b2b7a985b739f12e1f
Bishr Jones <Bishrjones6@gmail.com>
2022-05-01 23:03:39 -0700

    loading on show page

--- src/components/exercise/ShowExercise.js
+++ src/components/exercise/ShowExercise.js
@@ -2,3 +2,53 @@ import React, { useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
 import EditExercise from './EditExercise'
 import { removeExercise } from '../../api/excersise'
+
+const ShowExercise = (props) => {
+    const {user, exercise, workout, triggerRefresh} = props
+
+   const [showEditModal, setShowEditModal] = useState(false)
+
+   const deleteExercise = () => {
+       removeExercise(user, workout._id, exercise._id)
+            .then(()=> triggerRefresh())
+            .catch(console.error)
+   }
+
+   return (
+    <>
+        <Card className="m-2" style={{
+            width: "15rem",
+            alignment: "center",
+        }}>
+            <Card.Header>{exercise.name}</Card.Header>
+            <Card.Body>
+                <small>Quantity: {exercise.quantity}</small><br/>
+                <small>{exercise.description}</small><br/>
+                <Card.Footer >
+                </Card.Footer>
+                {
+                    user._id === workout.owner && 
+                        <>
+                            <Button variant="warning" onClick={() => setShowEditModal(true)}>
+                                Edit Exercise
+                            </Button>
+                            <Button onClick={() => destroyExercise()}variant="danger">
+                                Delete Exercise
+                            </Button>
+                        </>
+                }
+            </Card.Body>
+        </Card>
+        <EditExercise 
+            user={user}
+            workout={workout}
+            exercise={exercise}
+            show={showEditModal}
+            handleClose={() => setShowEditModal(false)}
+            triggerRefresh={triggerRefresh}
+        />
+    </>
+)
+}
+
+export default ShowExercise
--- src/components/workouts/ShowWorkouts.js
+++ src/components/workouts/ShowWorkouts.js
@@ -1,10 +1,23 @@
 import React, {useState, useEffect, useRef} from 'react'
 import { getOneWorkout, removeWorkout, updateWorkout } from '../../api/workout'
 import { useParams, useNavigate } from 'react-router-dom'
-import { useParams, useNavigate } from 'react-router-dom'
 import CommentForm from '../comments/CommentForm'
 import ShowComment from '../comments/ShowComment'
+import ShowExercise from '../exercise/ShowExercise'
+import AddExercise from '../exercise/AddExercise'
+// import EditWorkout from './EditWorkout'
 
 const ShowWorkouts = (props) => {
+    // setting all the damn states 
     const [workout, setWorkout] = useState(null)
+    const [modalOpen, setModalOpen] = useState(false)
+    const [ExerciseModalOpen, setExerciseModalOpen] = useState(false)
+    const [updated, setUpdated] = useState(false)
+    const navigate = useNavigate()
+
+    if (!workout) {
+        return <p>Loading...</p>
+    } 
 }
+
+export default ShowWorkouts
