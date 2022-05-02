190e3b91b47e562fdae68ae3a68928282a26b21a
Bishr Jones <Bishrjones6@gmail.com>
2022-05-01 21:27:48 -0700

    exercise form complete

---
+++ src/components/shared/ExerciseForm.js
@@ -0,0 +1,45 @@
+import { Form, Container, Buttom }from 'react-bootstrap'
+
+const ExerciseForm = (props) => {
+    const {exercise, handleChange, handleSubmit, heading} = props
+
+    return (
+        <Container className='justify-content-center'>
+            <h3>{heading}</h3>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    placeholder='Name of exercise.'
+                    value={exercise.name}
+                    name='name'
+                    onChange={handleChange}
+                />
+                <Form.Label>weight</Form.Label>
+                <Form.Control
+                    placeholder='45'
+                    value={exercise.weight}
+                    name='weight'
+                    type='number' 
+                    onChange={handleChange}
+                />
+                <Form.Label>Sets</Form.Label>
+                <Form.Control
+                    placeholder='4'
+                    value={exercise.sets}
+                    name='sets'
+                    type='number' 
+                    onChange={handleChange}
+                />
+                <Form.Label>Reps</Form.Label>
+                <Form.Control
+                    placeholder='12'
+                    value={exercise.reps}
+                    name='reps'
+                    type='number' 
+                    onChange={handleChange}
+                />
+
+            </Form>
+        </Container>
+    )
+}
--- src/components/workouts/ShowWorkouts.js
+++ src/components/workouts/ShowWorkouts.js
@@ -1,2 +1,10 @@
 import React, {useState, useEffect, useRef} from 'react'
 import { getOneWorkout, removeWorkout, updateWorkout } from '../../api/workout'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+import CommentForm from '../comments/CommentForm'
+import ShowComment from '../comments/ShowComment'
+
+const ShowWorkouts = (props) => {
+    const [workout, setWorkout] = useState(null)
+}
