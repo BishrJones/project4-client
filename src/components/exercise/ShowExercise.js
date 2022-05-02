f6c2bf2e060be1915896631da696a15d45a8d5a4
Bishr Jones <Bishrjones6@gmail.com>
2022-05-01 22:35:14 -0700

    delete exercise function working; and card showing on wourkouts

---
+++ src/components/exercise/ShowExercise.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react'
+import { Card, Button } from 'react-bootstrap'
+import EditExercise from './EditExercise'
+import { removeExercise } from '../../api/excersise'
